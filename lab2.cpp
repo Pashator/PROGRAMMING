81a873436e234196786cea3eb2a9094916c0e8e6
Pashator <karkamish35@gmail.com>
2025-09-27 16:27:22 +0200

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>;
+
+int main() {
+    long long h, a;
+    std::cin >> h >> a;
+    double S = (static_cast<double>(a) * h) / 2.0;
+    std::cout << S << std::endl;
+    return 0;
+}
