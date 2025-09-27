cd4350b22ba007ba3540f40efc5d72ccbc2e090f
Pashator <karkamish35@gmail.com>
2025-09-27 23:32:17 +0200

    Add files via upload

---
+++ lab4.1.cpp
@@ -0,0 +1,21 @@
+﻿//вариант 2
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int A, B, C;
+    std::cout << "Введите A, B и C: ";
+    std::cin >> A >> B >> C;
+
+    if (A % C == 0 && B % C == 0) {
+        std::cout << "(A + B) / C = " << (A + B) / C << std::endl;
+    }
+    else if (A % C == 0 && B % C != 0) {
+        std::cout << "A / C + B = " << A / C + B << std::endl;
+    }
+    else {
+        std::cout << "A - B - C = " << A - B - C << std::endl;
+    }
+
+    return 0;
+}
