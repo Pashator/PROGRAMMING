0e23b639166f5c9ecf765f942b0d617da9e64e59
Pashator <karkamish35@gmail.com>
2025-09-27 22:49:38 +0200

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int x, i;
+    std::cout << "Введите x и номер бита i: ";
+    std::cin >> x >> i;
+
+    x = x & ~(1 << i);
+    std::cout << "Результат: " << x << std::endl;
+
+    return 0;
+}
