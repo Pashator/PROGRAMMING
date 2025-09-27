9f421e5f921a08ab6426882166c36ad5b813b546
Pashator <karkamish35@gmail.com>
2025-09-27 23:33:09 +0200

    Add files via upload

---
+++ lab4.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int x;
+    std::cout << "Введите 1 или -1: ";
+    std::cin >> x;
+
+    if (x == -1) {
+        std::cout << "Negative number";
+    }
+    else if (x == 1) {
+        std::cout << "Positive number";
+    }
+    else {
+        std::cout << "Ошибка: можно использовать только -1 или 1";
+    }
+
+    return 0;
+}
