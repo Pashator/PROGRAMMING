511d961f659944489f1a8774f774ac49653c545c
Pashator <karkamish35@gmail.com>
2025-09-27 16:28:36 +0200

    Delete lab2.cpp

--- lab2.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>;
-
-int main() {
-    long long h, a;
-    std::cin >> h >> a;
-    double S = (static_cast<double>(a) * h) / 2.0;
-    std::cout << S << std::endl;
-    return 0;
-}
